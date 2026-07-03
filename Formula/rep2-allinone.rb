c75f64711b69b8c20884347b9d76398e033ad97e
fukumen <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-03 17:20:14 +0000

    chore: update formulas for v$(grep '^BASE_VERSION =' Makefile | awk '{print $3}')

--- Formula/rep2-allinone.rb
+++ Formula/rep2-allinone.rb
@@ -5,11 +5,11 @@ class Rep2Allinone < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://fukumen.github.io/rep2-allinone/macos/rep2-allinone-1.1.6-php8.5.7-caddy2.11.4+202607032325-macos-arm64.tar.gz"
-      sha256 "873b2956543369bd7376f2518e6c0f1f4f8d6f34bcc9e3cbdb786fa478a2d5fd"
+      url "https://fukumen.github.io/rep2-allinone/macos/rep2-allinone-1.1.6-php8.5.7-caddy2.11.4+202607040211-macos-arm64.tar.gz"
+      sha256 "2904ad36f1c2ac5c4b5779e9ff65676304002d29cd0ee073711c3f424df798cf"
     else
-      url "https://fukumen.github.io/rep2-allinone/macos/rep2-allinone-1.1.6-php8.5.7-caddy2.11.4+202607032325-macos-x86_64.tar.gz"
-      sha256 "4f2c9e8e337195436dfa6f4e237ebd56c80e50388d50b90db2ca29b8b74fd05c"
+      url "https://fukumen.github.io/rep2-allinone/macos/rep2-allinone-1.1.6-php8.5.7-caddy2.11.4+202607040211-macos-x86_64.tar.gz"
+      sha256 "cc8580a35b4687f289bee78f92dfa9faeefc6acf36ba17102b8a07482073db5f"
     end
   end
 
